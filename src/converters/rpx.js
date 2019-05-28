233e50e856b299b96ca15b278cea1341c3858f7e
taixw2 <254225961@qq.com>
2019-05-28 22:09:49 +0800

    chore: 引入 eslint

--- src/converters/rpx.js
+++ src/converters/rpx.js
@@ -1,19 +1,18 @@
 // normal value convert
 
-const utils = require('../utils/index')
-const opts = require('../utils/opts')
-const enums = require('../utils/enum')
+const utils = require('../utils/index');
+const opts = require('../utils/opts');
+const enums = require('../utils/enum');
 
-
-module.exports = function({ path, state, t, enter }, next) {
-	if (!enter) { return }
+module.exports = ({ path, state, enter }, next) => {
+  if (!enter) return next();
 
   const { key, value } = path.node;
-	const property = enums.widthProperties.filter(property => property === key.name)[0];
-	// 不需要转换
-	if (!property) { return }
+  const property = enums.widthProperties.filter(pro => pro === key.name)[0];
+  // 不需要转换
+  if (!property) return next();
 
-	path.node.value = utils.insertValue(value, opts(state.opts))
-
-	utils.inertRPXImport(path);
-}
+  path.node.value = utils.insertValue(value, opts(state.opts));
+  utils.inertRPXImport(path);
+  return next();
+};
