6006d786c4059d3d7f9385c4acc917191b08c520
taixw2 <254225961@qq.com>
2019-05-29 23:36:37 +0800

    feat: 删除 log

--- src/index.js
+++ src/index.js
@@ -2,28 +2,25 @@
 require('./converters/index');
 const compose = require('./utils/compose');
 
-module.exports = ({ types: t }) => {
-  console.info('react native style start');
-  return {
-    name: 'babel-plugin-react-native-style',
-    visitor: {
-      CallExpression: {
-        enter(path, state) {
-          compose.compose('CallExpression')({ path, t, state, enter: true });
-        },
-        exit(path, state) {
-          compose.compose('CallExpression')({ path, t, state, exit: true });
-        },
+module.exports = ({ types: t }) => ({
+  name: 'babel-plugin-react-native-style',
+  visitor: {
+    CallExpression: {
+      enter(path, state) {
+        compose.compose('CallExpression')({ path, t, state, enter: true });
       },
-
-      ObjectProperty: {
-        enter(path, state) {
-          compose.compose('ObjectProperty')({ path, t, state, enter: true });
-        },
-        exit(path, state) {
-          compose.compose('ObjectProperty')({ path, t, state, exit: true });
-        },
+      exit(path, state) {
+        compose.compose('CallExpression')({ path, t, state, exit: true });
       },
     },
-  };
-};
+
+    ObjectProperty: {
+      enter(path, state) {
+        compose.compose('ObjectProperty')({ path, t, state, enter: true });
+      },
+      exit(path, state) {
+        compose.compose('ObjectProperty')({ path, t, state, exit: true });
+      },
+    },
+  },
+});
