697c226bbde37d93c78d0fef4811a6a1c1d092a6
taixw2 <254225961@qq.com>
2019-06-24 09:42:17 +0800

    fix: insert import 改成 require

--- src/utils/bom.js
+++ src/utils/bom.js
@@ -21,37 +21,38 @@ module.exports = {
 
     this.insertFirst(
       path,
-      t.importDeclaration(
-        [t.importSpecifier(t.identifier(identifier), t.identifier(identifier))],
-        t.stringLiteral(source),
-      ),
+      t.variableDeclaration('var', [
+        t.variableDeclarator(
+          t.identifier(identifier),
+          t.memberExpression(
+            t.callExpression(t.identifier('require'), [t.stringLiteral(source)]),
+            t.identifier(identifier),
+          ),
+        ),
+      ]),
     );
   },
 
   insertRPXReference(path) {
-    this.insertImport(path, '__RPX', 'babel-plugin-react-native-style/src/runtime/index');
+    this.insertImport(path, '__RPX', 'babel-plugin-react-native-style-runtime');
   },
 
-  imported(path, name, source) {
+
+  /**
+   * 判断是否 require， 目前仅判断变量是否声明
+   *
+   * @param {*} path
+   * @param {*} name
+   * @returns
+   */
+  imported(path, name) {
     // TODO: 如果拥有相同的引用常量名称
     return _.find(this.program(path).node.body, (node) => {
-      if (!t.isImportDeclaration(node)) {
+      if (!t.isVariableDeclaration(node)) {
         return false;
       }
 
-      if (node.source.value !== source) {
-        return false;
-      }
-
-      return node.specifiers.some((spe) => {
-        if (t.isImportDefaultSpecifier(spe)) {
-          return spe.local.name === name;
-        }
-        if (t.isImportSpecifier(spe)) {
-          return spe.imported.name === name || spe.local.name === name;
-        }
-        return false;
-      });
+      return node.declarations[0].id.name === name;
     });
   },
 };
