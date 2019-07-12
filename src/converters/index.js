581cec62a8850ed9aa6af6e16eba3f3745a6e757
taixw2 <254225961@qq.com>
2019-07-12 23:31:21 +0800

    refactor: 删除部分文件

--- src/converters/index.js
+++
@@ -1,7 +0,0 @@
-const compose = require('../utils/compose');
-
-compose.use(require('./rpx'), 'ObjectProperty', 'StringLiteral');
-compose.use(require('./padding'), 'ObjectProperty');
-compose.use(require('./margin'), 'ObjectProperty');
-compose.use(require('./border'), 'ObjectProperty');
-compose.use(require('./border-radius'), 'ObjectProperty');
--- src/utils/opts.js
+++
@@ -1,7 +0,0 @@
-const _ = require('lodash');
-
-module.exports = (opts = {}) =>
-  _.defaultsDeep(opts, {
-    rpx: false,
-    designWidth: 750,
-  });
