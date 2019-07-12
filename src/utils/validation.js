241bd6695db05f5c4743424fcd39b74083368f41
taixw2 <254225961@qq.com>
2019-07-12 23:40:27 +0800

    feat: utils 精简

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,5 @@
 /* eslint-disable arrow-parens */
-const enums = require('./enum');
 const t = require('babel-types');
-const _ = require('lodash');
 
 module.exports = {
   plainObjectProperty(node) {
@@ -11,28 +9,7 @@ module.exports = {
     return true;
   },
 
-  value(value) {
-    if (value === 'auto') return true;
-    if (/^(\d+(\.\d+)?|\.\d+|\d+)?(%|pt|rpx)*$/.test(value)) return true;
-    return false;
-  },
-
-  borderStyle(borderStyle) {
-    return enums.borderStyle.includes(borderStyle);
-  },
-
-  color(color) {
-    const colorWithoutSpaces = color.replace(/\s/g, '');
-    return enums.colors.some((pattern) => {
-      if (_.isString(pattern)) {
-        return pattern === colorWithoutSpaces;
-      }
-      return pattern.test(colorWithoutSpaces);
-    });
-  },
-
-  pointProperty(pt) {
-    if (!pt) return false;
-    return enums.widthProperties.some((v) => v === pt.trim());
+  lengthUnit(value) {
+    return /^([+-]?(?:\d*\.)?\d+(?:[Ee][+-]?\d+)?(vh|vw|rpx|pt))$/.test(value);
   },
 };
