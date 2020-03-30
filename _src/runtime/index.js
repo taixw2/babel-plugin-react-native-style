12c5679799f661347dcdeeba8ed7575a76abe11b
mro <254225961@qq.com>
2020-03-30 16:58:47 +0800

    chore: remove old version

--- _src/runtime/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable no-underscore-dangle */
-const reactNative = require('react-native');
-
-const screenWidth = reactNative.Dimensions.get('window').width;
-
-exports.__RPX = (size, opts) =>
-  reactNative.PixelRatio.roundToNearestPixel((screenWidth / opts.designWidth) * parseInt(size, 10));
